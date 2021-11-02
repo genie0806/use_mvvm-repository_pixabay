112997925f59b3b737e25d1077aad1d0bd528553
genie0806 <genie0806@gmail.com>
2021-11-03 01:18:42 +0900

    freezed 라이브러리를 사용하여 union 기능 result에 사용하기

--- lib/domain/repository/result.dart
+++ lib/domain/repository/result.dart
@@ -1,14 +1,8 @@
-abstract class Result<T> {
-  factory Result.success(T data) = Success;
-  factory Result.error(Exception e) = Error;
-}
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'result.freezed.dart';
 
-class Success<T> implements Result<T> {
-  final T data;
-  Success(this.data);
-}
-
-class Error<T> implements Result<T> {
-  final Exception e;
-  Error(this.e);
+@freezed
+abstract class Result<T> with _$Result {
+  const factory Result.success(T data) = Success;
+  const factory Result.error(Exception e) = Error;
 }
